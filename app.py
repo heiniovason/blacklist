c88833dbe78736754099f611edf14b208a40b43b
Heini Leander Ovason <heini.ovason@gmail.com>
2018-03-07 15:33:38 +0100

    routing for BlacklistResource begun

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import falcon
 
 from test_resource import TestResource
+from blacklist_resource import BlacklistResource
  
 api = falcon.API()
 api.add_route('/test_resource', TestResource())
+api.add_route('/blacklist', BlacklistResource())
